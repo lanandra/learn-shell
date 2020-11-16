2a6d91cebb3017b11a282f4d3e3a5f1b05b813b2
Luthfi Anandra <luthfi.anandra@gmail.com>
2020-11-16 15:47:46 +0700

    Update auto_pull_run_docker.sh

--- auto_pull_run_docker.sh
+++ auto_pull_run_docker.sh
@@ -2,7 +2,7 @@
 #remove existing image and pull newer image
 #run container automatically using docker-compose
 
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'hello-world:latest') >> /tmp/pull_hello_world.out
-cd /home/lanandra/scripts ; docker-compose down >> /tmp/pull_hello_world
-docker pull hello-world:latest >> /tmp/pull_hello_world
-cd /home/lanandra/scripts ; docker-compose up -d >> /tmp/pull_hello_world
+docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'hello-world:latest')
+cd /home/lanandra/scripts ; docker-compose down
+docker pull hello-world:latest
+cd /home/lanandra/scripts ; docker-compose up -d
