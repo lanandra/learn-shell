927f398ed6f72f38b42faf427ace9837e80ab924
Luthfi Anandra <luthfi.anandra@gmail.com>
2020-11-21 20:13:44 +0700

    Update auto_pull_run_docker.sh

--- auto_pull_run_docker.sh
+++ auto_pull_run_docker.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-#remove existing image and pull newer image
-#run container automatically using docker-compose
+# !/bin/bash
+# remove existing images based on repositary name and tag
+# stop all containers that defined in docker-compose
+# pull newer images
+# run container automatically using docker-compose
+# please change the docker images and scripts path as necessary
 
 docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'hello-world:latest')
 cd /home/lanandra/scripts ; docker-compose down
