1a67eb26bad7b9c2ad39b416939f240820f6b9a5
Luthfi Anandra <luthfi.anandra@gmail.com>
2021-02-22 21:30:28 +0700

    Update db_backup_s3.sh

--- db_backup_s3.sh
+++ db_backup_s3.sh
@@ -4,7 +4,7 @@
 # copy to s3 with acl set to bucket owner full control, sse encryption, storage class using onezone_ia and using spesific profile
 # get date and print backup date at output
 
-DATENOW=$(date);
+DATENOW=$(date +"%A, %d-%m-%Y %H:%M:%S");
 aws s3 cp your_sql.sql s3://your_path/your_sql.sql --acl bucket-owner-full-control --sse --storage-class ONEZONE_IA --profile your_profile;
 aws s3 cp your_mongo.dump s3://your_path/your_mongo.dump --acl bucket-owner-full-control --sse --storage-class ONEZONE_IA --profile your_profile;
 echo "Backup has been done at $DATENOW"
