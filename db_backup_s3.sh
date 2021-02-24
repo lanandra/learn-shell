21711146f629c70f557ad0c3b958913b536d8619
Luthfi Anandra <luthfi.anandra@gmail.com>
2021-02-24 11:37:17 +0700

    Update db_backup_s3.sh

--- db_backup_s3.sh
+++ db_backup_s3.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# export shell and path to be used on cron job
+# define .profile and path to script on crontab
+# * * * * * . $HOME/.profile; /path/db_backup_s3.sh
+export SHELL=/bin/bash
+export PATH=/usr/local/sbain:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # backup your local backup db file to aws s3 using aws cli
 # copy to s3 with acl set to bucket owner full control, sse encryption, storage class using onezone_ia and using spesific profile
 # get date and print backup date at output
-
 DATENOW=$(date +"%A, %d-%m-%Y %H:%M:%S");
 aws s3 cp your_sql.sql s3://your_path/your_sql.sql --acl bucket-owner-full-control --sse --storage-class ONEZONE_IA --profile your_profile;
 aws s3 cp your_mongo.dump s3://your_path/your_mongo.dump --acl bucket-owner-full-control --sse --storage-class ONEZONE_IA --profile your_profile;
