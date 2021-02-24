b0062e3283fc3e222b6eebe391e1dd7be0b0bf84
Luthfi Anandra <luthfi.anandra@gmail.com>
2021-02-24 11:39:04 +0700

    Update db_backup_local.sh

--- db_backup_local.sh
+++ db_backup_local.sh
@@ -3,7 +3,7 @@
 # backup mysql db and mongo db that running on docker container
 # get date and print date on output
 
-DATENOW=$(date +"%A, %d-%m-%Y %H:%M:%S");
+DATENOW=$(date +"%A, %d-%m-%Y %H:%M:%S %Z");
 docker exec your_mysql_container /usr/bin/mysqldump -u your_user --password=your_password your_db > your_path/your_file.sql;
 docker exec your_mongo_container sh -c 'mongodump --archive' > your_path/your_file.dump;
 echo "Backup has been done at $DATENOW"
